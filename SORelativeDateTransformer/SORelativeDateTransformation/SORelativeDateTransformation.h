db77b94e77139d38e5f63726bee06d5359cb633e
Stephen Anthony <steve@darjeelingapps.com>
2016-06-10 19:26:29 +0100

    Add public framework target

---
+++ SORelativeDateTransformer/SORelativeDateTransformation/SORelativeDateTransformation.h
@@ -0,0 +1,17 @@
+//
+//  SORelativeDateTransformation.h
+//  SORelativeDateTransformation
+//
+//  Created by Stephen Anthony on 10/06/2016.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SORelativeDateTransformation.
+FOUNDATION_EXPORT double SORelativeDateTransformationVersionNumber;
+
+//! Project version string for SORelativeDateTransformation.
+FOUNDATION_EXPORT const unsigned char SORelativeDateTransformationVersionString[];
+
+#import <SORelativeDateTransformation/SORelativeDateTransformer.h>
